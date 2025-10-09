82ede40531341203131d00cafe187fd1d4252ba6
NgPrapthi <148470425+NgPrapthi@users.noreply.github.com>
2025-10-09 22:15:16 +0530

    Update Journal.js

--- database/Journal.js
+++ database/Journal.js
@@ -1,1 +1,22 @@
 
+const mongoose = require('mongoose');
+
+const JournalSchema = new mongoose.Schema({
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student',
+    required: true,
+  },
+  entryText: {
+    type: String,
+    required: true,
+  },
+  entryDate: {
+    type: Date,
+    default: Date.now,
+  },
+  tags: [String]
+});
+
+module.exports = mongoose.model('Journal', JournalSchema);
+
