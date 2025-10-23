d3f77a17bd67450fa6009495a0cd87b5aa4ead83
Shivaji-M-P <23c62.shivaji@sjec.ac.in>
2025-10-23 23:40:04 +0530

    backend code pushed

--- backend/routes/emotionRoutes.js
+++ backend/routes/emotionRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { addEmotion, getEmotions } from "../controllers/emotionController.js";
+
+import { addEmotion, getEmotions } from "../controlletrs/emotionController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
